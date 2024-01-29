ada38feb5a3e407c69cfe393bf25f32c9bf87797
amita <amitasher4@gmail.com>
2024-01-29 12:29:20 +0200

    update p

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-function Button({ label, iconUrl }) {
+function Button({ label, iconUrl, backgroundColor, textColor, borderColor }) {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montsrrat text-lg leading-none bg-coral-red rounded-full text-white border-coarl-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montsrrat text-lg leading-none ${
+        backgroundColor
+          ? `${backgroundColor}  ${textColor} ${borderColor}`
+          : "bg-coral-red  text-white border-coarl-red"
+      } rounded-full  "w-full"}`}
+    >
       {label}
 
       <img
